e28de4e4bc8cff55ae9c9aec8c76d64af93a9fc8
SrVallejo <l.vallejo.28@gmail.com>
2022-07-20 13:05:44 +0200

    clase coches no va muy allá

---
+++ PruebaClasses/ClaseCoche.py
@@ -0,0 +1,19 @@
+'''
+Clase coche prueba
+'''
+
+class Coche:
+
+    tires = 4
+    brand = " "
+    model = " "
+    color = " "
+
+    def __init__(self,brand, model, color):
+        self.brand = brand
+        self.model = "Zafira"
+        self.color = "Grey"
+
+    def imprimir():
+
+        print("Que coche más bonito")
---
+++ PruebaClasses/Programa_coche.py
@@ -0,0 +1,5 @@
+from ClaseCoche import ClaseCoche
+
+mi_coche = ClaseCoche("Opel","Astra","Grey")
+mi_coche.imprimir()
+
