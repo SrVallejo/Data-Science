4c72d6e26c23cdba8c892984faebc83973576a94
SrVallejo <l.vallejo.28@gmail.com>
2022-09-06 10:28:37 +0200

    Ejercicios 6 completado

--- Basico/Ejercicios_6.py
+++ Basico/Ejercicios_6.py
@@ -1,5 +1,7 @@
 import os
 clear = lambda: os.system('cls')
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # EJERCICIO 1
 
@@ -104,18 +106,101 @@ def ejercicio_4():
     list = sort_list_desc(list)
     print(list)
 
-ejercicio_4()
-
 # EJERCICIO 5
-"""
-    Pendiente
-"""
+
+# Escribe el código necesario en Python para:
+
+# almacenar con una lista de nombre "modulos" los siguientes materias de un programa de Ciencia de datos:
+
+# Big Data, Python, Algoritmos, Machine Learning, Deep Learning, NLP.
+
+# 1) Imprime el listado y imprime los valores de uno en uno
+
+# 2) Dentro de ese grupo de materias, existen unas materias que son básicas en todos los programas, y que forman la base de conocimientos inciales para afrontar con éxito el resto de un programa.
+
+# Las mismas son: Python y Algoritmos (aunque en la práctica hay más cosas).
+
+# Se pide almacenar las mismas en un listado secundario, de nombre: "esenciales"
+
+# Imprime ese listado al terminar de almacenarlos
+
+# 3) Crea un Dataframe, de nombre "df" con esa infromación en base a la siguiente relación de módulos y horas de clase.
+
+# módulo: Big Data, Python, Algoritmos, Machine Learning, deep Learning, NLP
+
+# horas: 25, 15, 5, 15, 5, 10
+
+# 4) De ese dataframe, selecciona la columna "horas" e imprimelas
+
+# 5) Muestra el gráfico (plot) para la columna "horas"
+
+# 6) Selecciona solamente las materias aquellas que tienen 20 o más horas de dedicación
+
+# 7) Selecciona solamente las materias aquellas que tienen menos 10 horas de dedicación
+
+# 8) Selecciona solamente las materias (si es posible) aquellas que tienen mas de 26 horas de dedicación
+
+# 9) Apendiza una nueva columna llamada "docente" con el nombre del instructor de la materia. Y cuyos nombres serán: 
+# Erique, Susana, Juan, Ana, Laura, Patricia, Pedro
+
+def mas_horas(df,horas):
+    df_aux = df[df["Horas"] >= horas]
+
+    if df_aux.size != 0: print(df_aux[["Modulos"]])
+    else: print("No hay registros")
+    print("")
+
+def ejercicio_5():
+    #1)
+    modulos = ["Big Data", "Python", "Algoritmos", "Machine Learning", "Deep Learning", "NLP"]
+    print(f"1)\nLista modulos:\n{modulos}\n")
+
+    #2)
+    esenciales = ["Python","Algoritmos"]
+    print(f"2)\nLista esenciales:\n{esenciales}\n")
+
+    #3)
+    horas = [25, 15, 5, 15, 5, 10]
+    df = pd.DataFrame({"Modulos":modulos,"Horas":horas})
+    print(f"2)\nData frame:\n{df}\n")
+
+    #4)
+    print(f"4)\nColumna Horas:\n{df.Horas}\n")
+
+    #5)
+
+    figure = df.Horas.plot(kind="bar")
+    plt.show()
+
+    #6)
+    horas = 20
+    print(f"6)\nModulos con más de {horas} horas:\n")
+    mas_horas(df,horas)
+
+    #7)
+    horas = 10
+    print(f"7)\nModulos con más de {horas} horas:\n")
+    mas_horas(df,horas)
+
+    #8)
+    horas = 26
+    print(f"8)\nModulos con más de {horas} horas:\n")
+    mas_horas(df,horas)
+
+    #9)
+    instructores = ["Erique", "Susana", "Juan", "Ana", "Laura", "Patricia"]
+    df["Docentes"] = instructores
+    print(f"9)\nColumna apendizada:\n{df}\n")
+
+
+#MENU Y MAIN
 
 def pintar_menu():
     print("Opcion 1: Mínimo lista")
     print("Opcion 2: Máximo Lista")
     print("Opcion 3: Orden ascendente")
     print("Opcion 4: Orden descendente")
+    print("Opcion 5: Data frame")
     print("Opcion -1: Salir")
 
 def main():
@@ -140,6 +225,10 @@ def main():
 
         elif option == "4":
             ejercicio_4()
+            input("Continuar...")
+
+        elif option == "5":
+            ejercicio_5()
             input("Continuar...")
 
         elif option == "-1":
