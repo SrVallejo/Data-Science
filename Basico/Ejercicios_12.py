61ae772396ce5c512f24334d51e57dd73b8d4f36
SrVallejo <l.vallejo.28@gmail.com>
2022-09-07 12:09:50 +0200

    Ejercicios 12: Enunciado

---
+++ Basico/Ejercicios_12.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+#EJERCICIO 1
+'''1)
+
+Haz un programa que calcule los múltiplos de 3
+
+    Para ello primero primero debe preguntarse al usuario cuántos múltiplos desea añadir.
+
+    Nota: Puedes usar un bucle while si lo deseas
+
+2)
+
+Haz lo mismo con np.arange
+'''
+
