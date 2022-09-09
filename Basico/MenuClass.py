0ba0699a917cdb7c7a6550097368e00d78a345aa
SrVallejo <l.vallejo.28@gmail.com>
2022-09-09 13:41:57 +0200

    Commented

--- Basico/MenuClass.py
+++ Basico/MenuClass.py
@@ -6,6 +6,11 @@ class menu:
     options_strings = []
     num_opt = 0
 
+    #Builder
+    '''Functions_list -> List of the functions the menu is going to call
+    opt_list -> Lisf ot the strings that describe that functions and the menu is going to print
+    Both list need to have the same size, and the function and his description in the same position
+    '''
     def __init__(self,functions_list,opt_list):
         if len(functions_list) != len(opt_list):
             print("Error: opt_list length != functions_list length")
@@ -14,11 +19,15 @@ class menu:
         self.options_strings = opt_list
         self.num_opt = len(functions_list)
 
+    #Funtion that prints the menu with the options specified in the building
     def print_menu(self):
         for i in range(self.num_opt):
             print(f"Option {i+1}: {self.options_strings[i]}")
         print("Option -1: Exit")
-        
+
+
+    #Function that prints the menu and ask the user for the desired option
+    # Return the function that is going to be executed, or -1 to exit.    
     def select_menu(self):
         while True:
             clear()
@@ -37,3 +46,19 @@ class menu:
             except:
                 print("Invalid option")
                 input("Try again...")
+
+
+#Exemple code to run this class in your script
+'''
+    functions =[func_1,func_2]
+    options = ["Function 1","Function 2"]
+    mymenu = menu(functions,options) 
+
+    while True:
+        selected_function = mymenu.select_menu()
+        if selected_function == -1: break
+        else: selected_function()
+        input("Continue...")
+    
+    input("Thanks for using this menu")
+'''
