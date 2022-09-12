e5a3badd64a56dddfe28302ac64573e218ac6efb
SrVallejo <l.vallejo.28@gmail.com>
2022-09-12 13:12:49 +0200

    fast and slow fibonacci

--- Basico/Recursive_fibonacci.py
+++ Basico/Recursive_fibonacci.py
@@ -1,14 +1,24 @@
-def fibonacci(n):
-    
+num = 10
+
+def fast_fibonacci(n):
     if n == 0:
         return (0,1)
     else:
-        a,b = fibonacci(n//2)
+        a,b = fast_fibonacci(n//2)
         c = a *(b*2-a)
         d = a * a + b * b
 
         if (n%2) == 0: return (c,d)
         else: return (d, c+d)
 
-print (fibonacci(7))
+print (fast_fibonacci(num))
+
+def fibonacci(n):
+
+    if n < 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print (fibonacci(num)) 
     
