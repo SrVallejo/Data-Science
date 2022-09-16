54b9e945b6822c87cf231a154fe3eae2b177a4a6
SrVallejo <l.vallejo.28@gmail.com>
2022-09-16 14:43:23 +0200

    cleaning

--- POO/ClassHuman.py
+++ POO/ClassHuman.py
@@ -1,17 +1,3 @@
-from typing import Type
-
-'''
-6) Crea una clase persona. Sus atributos deben ser su nombre y su edad. Además crea un método cumpleaños, 
-que aumente en 1 la edad de la persona.
-
-7) Para la clase anterior definir el método str. Debe retornar al menos el nombre de la persona.
-
-8) Extender la clase persona agregando un atributo saldo y un método transferencia(self, persona2, monto). 
-El saldo representa el dinero que tiene la persona. 
-El método transferencia hace que la Persona que llama el método le transfiera la cantidad monto al objeto persona2. 
-Si no tiene el dinero suficiente no se ejecuta la acción.
-'''
-
 class Human:
     money = 0
 
